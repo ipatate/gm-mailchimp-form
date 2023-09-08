f65e1a5d316f6777e25a137333e0bf9a6279ce56
ipatate <faramazpat@gmail.com>
2023-09-08 10:30:58 +0200

    build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '319ac4095b313eb809bc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill', 'wp-server-side-render'), 'version' => '0baccf9be599a2ccded3');
